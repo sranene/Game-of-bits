53d53658038e0be13c7046444a2cebf7d20c32d5
Inês Marques <a22001936@alunos.ulht.pt>
2021-12-10 18:18:48 +0000

    tentamos fazer o cicloinfinito

--- src/pt/ulusofona/lp2/deisiGreatGame/Loop.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/Loop.java
@@ -10,7 +10,25 @@ public class Loop extends Abyss{
 
     @Override
     public String react(Programmer programmer, int dado, TreeMap<Integer,Square> boardMap) {
-        programmer.setLoop(true);
+
+        if (boardMap.get(pos).getProgrammers().size() > 1) {
+            int count = 0;
+            for (Tool tool : boardMap.get(pos).getProgrammers().get(boardMap.get(pos).getProgrammers().size() - 1).getTools()) {
+                if (tool.getTitle().equals("Tratamento de Excepções")) {
+                    count++;
+                    programmer.removeTool(tool);
+                }
+            }
+            if (count == 0) {
+                programmer.setLoop(true);
+                boardMap.get(pos).getProgrammers().get(0).setLoop(false);
+                return "tas preso";
+            }
+            return "safaste-te";
+        } else {
+            programmer.setLoop(true);
+        }
         return "Loop";
     }
+
 }
