2c21f3937c7554841694e601692faf879d9a0659
Robert Cachapa <a22006023@alunos.ulht.pt>
2022-01-04 11:18:09 +0000

    Save and Load Game v4

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -5,11 +5,11 @@ enum class CommandType{
     POST
 }
 
-fun router() : Function1<CommandType,Function2<GameManager,List<String>,String>?>{
+fun router() : Function1<CommandType,Function2<GameManager,List<String>,String?>?>{
     return ::depoisDoRouter
 }
 
- fun depoisDoRouter(type: CommandType?) : Function2<GameManager,List<String>,String>? {
+ fun depoisDoRouter(type: CommandType?) : Function2<GameManager,List<String>,String?>? {
      if(type == CommandType.GET){
          return ::comandoGet
      }else if (type == CommandType.POST){
@@ -18,11 +18,45 @@ fun router() : Function1<CommandType,Function2<GameManager,List<String>,String>?
      return null
  }
 
- fun comandoGet(game: GameManager, args :List<String>) : String {
-     return "1"
+ fun comandoGet(game: GameManager, args :List<String>) : String? {
+     when(args[0]){
+             "PLAYER" -> return::getPlayer.invoke(game,args)
+             "PLAYERS_BY_LANGUAGE" -> return::getPlayersByLanguage.invoke(game,args)
+             "POLYGLOTS" -> return::getPolyglots.invoke(game,args)
+             "MOST_USED_POSITIONS" -> return::mostUsedPositions.invoke(game,args)
+             "MOST_USED_ABYSSES" -> return::mostUsedAbysses.invoke(game,args)
+     }
+     return null
+ }
+ fun getPlayer(game: GameManager,args : List<String>) : String? {
+
+     return null
  }
 
- fun comandoPost(game: GameManager, args : List<String>):String {
-    return "1"
+
+
+
+ fun mostUsedAbysses(game: GameManager,args : List<String>) : String?{
+    return null
+ }
+
+ fun mostUsedPositions(game: GameManager,args : List<String>) : String?{
+    return null
+ }
+
+ fun getPolyglots(game: GameManager,args : List<String>) : String?{
+    return null
+ }
+
+ fun getPlayersByLanguage(game: GameManager,args: List<String>) : String?{
+    return null
+ }
+
+ fun comandoPost(game: GameManager, args : List<String>) : String? {
+     when(args[0]){
+         "MOVE" -> return::postMove.invoke(game,args)
+         "ABYSS" -> return::postAbyss.invoke(game,args)
+     }
+     return null
  }
 
